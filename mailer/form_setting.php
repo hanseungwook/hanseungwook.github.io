eebbe2d8ae1a9d2bddf1bbd0fe460b581a88515a
Seungwook Han <sh3264@coumbia.edu>
2021-12-06 20:24:57 -0500

    New theme

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "Contacting from your webpage'"; //letter subject
+	$to = 'sh3264@columbia.edu'; // Enter Your E-mail
+	$from = 'sh3264@columbia.edu'; // Admin e-mail
+	$fromName = 'Seungwook Han'; // Your company name
+	$charset = 'UTF-8';
+?>
